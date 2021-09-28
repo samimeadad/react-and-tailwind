0e6ad1cdc6a2f24fd38dca502e0c03f2dd5dfc00
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-09-29 02:19:26 +0600

    tailwind installed and used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="bg-yellow-200 text-5xl p-5 hover:bg-blue-400 hover:text-red-700">This is a tailwind styled paragraph</p>
+      <p className="text-primary">This is my custom styled tailwind class</p>
+      <p className="text-custom">This is my custom styled tailwind class</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: [ './src/**/*.{js,jsx,ts,tsx}', './public/index.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: {
+      'primary': 'blue',
+      'secondary': 'green',
+      'danger': 'red',
+      'custom': 'goldenrod',
+    },
     extend: {},
   },
   variants: {
