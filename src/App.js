30a9570cb57f58ca03dfc520ccd0aeb12c56fab3
samimeadad <sami.meadad@gmail.com>
2021-10-23 19:37:22 +0600

    image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import localImage from '../src/images/IVF-ET.png';
 
 function App () {
   return (
     <div className="App">
+
+      <img src={ localImage } alt="" />
+      <img src="https://chennaifertilitycenter.com/assets/images/IVF-ET.png" alt="" />
       <p className="bg-yellow-200 text-5xl p-5 hover:bg-blue-400 hover:text-red-700">This is a tailwind styled paragraph</p>
       <p className="text-primary">This is my custom styled tailwind class</p>
       <p className="text-custom">This is my custom styled tailwind class</p>
